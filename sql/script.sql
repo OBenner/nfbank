1fd2835415ffed492dd2b7d98c5ca6d0db916100
OBenner <omyagkov@bk.ru>
2018-01-23 23:27:33 +0400

    add script an gitignore

---
+++ sql/script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE clientinformation
+(
+  id            INT AUTO_INCREMENT
+    PRIMARY KEY,
+  name          VARCHAR(45) NULL,
+  lastname      VARCHAR(45) NULL,
+  birth         VARCHAR(45) NULL,
+  password      VARCHAR(45) NULL,
+  clientaccount VARCHAR(45) NULL
+)
